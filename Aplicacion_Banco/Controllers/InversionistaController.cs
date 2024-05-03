30963a1dbc6ca2293667b2b220fdf2aa29d19f23
Darwin Tantalean <dtantaleanvalverde@gmail.com>
2024-05-03 17:56:28 -0500

    creacion de inversionista y prestamista

---
+++ Aplicacion_Banco/Controllers/InversionistaController.cs
@@ -0,0 +1,32 @@
+﻿using Aplicacion_Banco.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion_Banco.Controllers
+{
+    public class InversionistaController : Controller
+    {
+        private readonly APLICACION_BANCOContext _db;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public InversionistaController(APLICACION_BANCOContext db, IHttpContextAccessor httpContextAccessor)
+        {
+            _db = db;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CrearJefePrestamista(Usuario model)
+        {
+            int? userId = _httpContextAccessor.HttpContext.Session.GetInt32("UserId");
+
+            model.IdRol = 2;
+            model.IdUsuarioCreador = userId;
+            _db.Usuarios.Add(model);
+            _db.SaveChanges();
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ Aplicacion_Banco/Controllers/PrestamistaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion_Banco.Controllers
+{
+    public class PrestamistaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
