1f391460a2dd7efb3ea06410284a8dad8babf87c
axelcv12 <i202121233@cibertec.edu.pe>
2024-05-03 17:35:51 -0500

    Agregar archivos de proyecto.

---
+++ Aplicacion_Banco/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aplicacion_Banco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
