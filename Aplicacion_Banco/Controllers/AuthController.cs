06728ec5d597efb2a32c5ed26ba0fb6a5a9207fd
Victor Manuel <galarzamartinezv@gmail.com>
2024-05-03 17:52:13 -0500

    Controlador Auth y controlador Prestatario

---
+++ Aplicacion_Banco/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using Aplicacion_Banco.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion_Banco.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<PrestatarioController> _logger;
+        private readonly APLICACION_BANCOContext _db;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthController(ILogger<PrestatarioController> logger, APLICACION_BANCOContext db, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _db = db;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Login()
+        {   
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ValidarUsuario(Usuario model)
+        {
+            Usuario usuario = _db.Usuarios.FirstOrDefault(u => u.Correo == model.Correo && u.Contrasena == model.Contrasena);
+            if(usuario == null)
+            {
+                return View("Login", model);
+            }
+            _httpContextAccessor.HttpContext.Session.SetInt32("UserId", usuario.Id);
+                        
+            if (usuario.IdRol == 1)
+            {
+                return RedirectToAction("Index", "Inversionista");
+            }
+
+            if (usuario.IdRol == 2)
+            {
+                return RedirectToAction("Index", "JefePrestamista");
+            }
+
+            if (usuario.IdRol == 3)
+            {
+                return RedirectToAction("Index", "Prestamista");
+            }
+
+            if (usuario.IdRol == 4)
+            {
+                return RedirectToAction("Index", "Prestatario");
+            }
+
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public IActionResult Register(Usuario model)
+        {
+            model.IdRol = 4;
+            _db.Usuarios.Add(model);
+            _db.SaveChanges();
+
+            _httpContextAccessor.HttpContext.Session.SetInt32("UserId", model.Id);
+            return View("Registrar", model);
+        }
+
+    }
+}
---
+++ Aplicacion_Banco/Controllers/PrestatarioController.cs
@@ -0,0 +1,106 @@
+﻿using Aplicacion_Banco.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Aplicacion_Banco.Controllers
+{
+    public class PrestatarioController : Controller
+    {
+        private readonly ILogger<PrestatarioController> _logger;
+        private readonly APLICACION_BANCOContext _db;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public PrestatarioController(ILogger<PrestatarioController> logger, APLICACION_BANCOContext db, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _db = db;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            int? userId = _httpContextAccessor.HttpContext.Session.GetInt32("UserId");
+
+            VistaPrestamo model = new VistaPrestamo();
+            model.Prestamos = _db.Prestamos
+                .Where(p => p.IdPrestatario == userId)
+                .Include(p => p.IdMontoNavigation)
+                .Include(p => p.IdPrestamistaNavigation)
+                .Include(p => p.IdDuracionNavigation)
+                .Include(p => p.IdMetodoPagoNavigation)
+                .ToList();
+
+            model.Montos = _db.Montos.ToList();
+            model.Duraciones = _db.Duracions.ToList();
+            model.Metodopagos = _db.Metodopagos.ToList();
+
+            model.Prestamistas = _db.Usuarios.Where(u => u.IdRol == 3).ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult CrearPrestamo(VistaPrestamo vistaPrestamo)
+        {
+            int? userId = _httpContextAccessor.HttpContext.Session.GetInt32("UserId");
+
+            Prestamo prestamo = vistaPrestamo.Prestamo;
+            prestamo.Estado = "Pendiente";
+            prestamo.FechaFin = SumarDiasLaborales(prestamo.FechaInicio.GetValueOrDefault(), 30);
+            prestamo.IdPrestatario = userId;
+            prestamo.PagoDiario = ObtenerPagoDiario(prestamo);
+
+            _db.Prestamos.Add(prestamo);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Detalle(int id)
+        {
+            Prestamo prestamo = _db.Prestamos.Where(p => p.Id == id)
+                .Include(p => p.IdPrestatarioNavigation)
+                .Include(p => p.IdMontoNavigation)
+                .Include(p => p.IdDuracionNavigation)
+                .FirstOrDefault();
+
+            return View(prestamo);
+        }
+
+        public decimal? ObtenerPagoDiario(Prestamo prestamo)
+        {
+            decimal? montoDiario;
+
+            decimal? monto = _db.Montos.First(m => m.Id == prestamo.IdMonto).Valor;
+            decimal? interes = _db.Duracions.First(d => d.Id == prestamo.IdDuracion).Interes;
+            int? diasDuracion = _db.Duracions.First(d => d.Id == prestamo.IdDuracion).Duracion1;
+
+            decimal? montoConInteres = monto + (monto * interes);
+
+            montoDiario = montoConInteres / diasDuracion;
+                        
+            return Math.Round(montoDiario.GetValueOrDefault(), 3);
+        }
+
+        public DateTime SumarDiasLaborales(DateTime fechaInicio, int dias)
+        {
+            DateTime fechaFin = fechaInicio;
+            int diasLaborables = 0;
+            int diasAgregados = 0;
+
+            while (diasAgregados < dias)
+            {
+                fechaFin = fechaFin.AddDays(1);
+                if (fechaFin.DayOfWeek != DayOfWeek.Saturday && fechaFin.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    diasLaborables++;
+                    diasAgregados++;
+                }
+            }
+
+            return fechaFin;
+        }
+
+    }
+}
