06f94fc1d81f02b6948a9e74c3e65a176691625c
ACER <ACER@DESKTOP-81UCOTI>
2024-05-03 19:15:59 -0500

    Creacion de Control de jefe de prestamista

---
+++ Aplicacion_Banco/Controllers/JefePrestamistaController.cs
@@ -0,0 +1,32 @@
+﻿using Aplicacion_Banco.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion_Banco.Controllers
+{
+    public class JefePrestamistaController : Controller
+    {
+        private readonly APLICACION_BANCOContext _db;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public JefePrestamistaController(APLICACION_BANCOContext db, IHttpContextAccessor httpContextAccessor)
+        {
+            _db = db;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CrearPrestamista(Usuario model)
+        {
+            int? userId = _httpContextAccessor.HttpContext.Session.GetInt32("UserId");
+
+            model.IdRol = 3;
+            model.IdUsuarioCreador = userId;
+            _db.Usuarios.Add(model);
+            _db.SaveChanges();
+
+            return View("Index", model);
+        }
+    }
+}
